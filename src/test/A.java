03b53375d53403c5127c3258fa1d729373b10128
天字无名 <shubaowang@126.com>
2014-05-14 23:39:48 +0800

    master：2

--- src/test/A.java
+++ src/test/A.java
@@ -5,11 +5,8 @@ public class A {
 	public static void main(String[] args) {
 
 		System.out.println("abc");
-<<<<<<< HEAD
 		System.out.println("master: edit B");
-=======
 		System.out.println("develop edit A");
->>>>>>> develop
 	}
 
 }
