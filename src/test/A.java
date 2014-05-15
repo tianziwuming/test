7ad58ab5aabd4e2292696e5acf704f4768a565b6
wangshubao <wangshubao@lashou-inc.com>
2014-05-15 14:35:15 +0800

    abc

--- src/test/A.java
+++ src/test/A.java
@@ -10,5 +10,29 @@ public class A {
 		System.out.println("develop edit A 2");
 		System.out.println("dev3: 3");
 	}
+	
+	private int id;
+	private String name;
+	private String desc;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
 
+	
 }
